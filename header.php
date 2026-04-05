0faad7821e8bbfe6263d324f0dfeae48970a17c6
Mirka71 <200627729@student.georgianc.on.ca>
2026-04-05 17:27:49 -0400

    Linked stylesheet

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <head>
+    <!-- Character encoding -->
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <!-- Responsive layout -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <?php wp_head(); ?>
+
+    <!-- stylesheet link-->
+     <link rel="stylesheet" href="<?php echo esc_url(home_url('wp-content/themes/vinylrecordshop/style.css')); ?>">
+     
