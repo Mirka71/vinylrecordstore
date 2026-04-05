106198b1662db4ba1d9c2f33fc3f005dd2fbb213
Mirka71 <200627729@student.georgianc.on.ca>
2026-04-05 17:43:57 -0400

    found photos, and created header

--- header.php
+++ header.php
@@ -1,13 +1,23 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
     <head>
-    <!-- Character encoding -->
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <!-- Responsive layout -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Character encoding -->
+        <meta charset="<?php bloginfo('charset'); ?>">
+        <!-- Responsive layout -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <?php wp_head(); ?>
+        <?php wp_head(); ?>
 
-    <!-- stylesheet link-->
-     <link rel="stylesheet" href="<?php echo esc_url(home_url('wp-content/themes/vinylrecordshop/style.css')); ?>">
-     
+        <!-- stylesheet link-->
+        <link rel="stylesheet" href="<?php echo esc_url(home_url('wp-content/themes/vinylrecordshop/style.css')); ?>">
+    </head>
+
+    <body <?php body_class(); ?>>
+        <!-- Header -->
+        <header class="main-header navbar">
+            <a href="<?php echo esc_url(home_url()); ?>">
+                <!-- Image -->
+                <img src="<?php echo esc_url(home_url('wp-content/themes/vinylrecordshop/orangevinyl.avif')); ?>" alt="Image of an orange vinyl record.">
+            </a>      
+
+            
