a3059a339b99ce4f30342b7ee21ed2fd17d7be85
Mirka71 <200627729@student.georgianc.on.ca>
2026-04-06 02:17:20 -0400

    added stylesheet, created post type for records and labels

--- functions.php
+++ functions.php
@@ -11,3 +11,21 @@ add_action('after_setup_theme', 'theme_setup');
 
 // adding featured img to posts
 add_theme_support('post-thumbnails');
+
+// add stylesheet to function
+function my_theme_enqueue_assets(){
+    get_template_directory_uri() . '/style.css'
+}
+
+// create post type for records
+function post_type_records(){
+// labels that will appear in the menu
+$labels = array(
+    'name' => 'Records',
+    'singular_name' => 'Record',
+    'add_new' => 'Add New Record',
+    'all_items' => 'All Records',
+    'menu_name' => 'Vinyl Records'
+);
+register_post_type('records', $args);
+}
