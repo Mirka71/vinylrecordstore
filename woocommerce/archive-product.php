1b12eb68b30bd17bbd734fd9bf9a9cbd93a9b3f6
Mirka71 <200627729@student.georgianc.on.ca>
2026-04-07 17:16:14 -0400

    fixed bugs and added woocommerce template

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -7,18 +7,19 @@ get_header('shop');
 
 // use shop id to get featured image
 $shop_id = wc_get_page_id('shop');
-$shopFeaturedImg = wp_get_attachment_image_src(get_post_thmbnail_id($shop_Id), 'full');
+$shopFeaturedImg = wp_get_attachment_image_src(get_post_thumbnail_id($shop_id), 'full');
 $bg_url = $shopFeaturedImg ? $shopFeaturedImg[0] : '';
 ?>
 
-// for hero image and added classes to later style in stylesheet
+<!-- for hero image and added classes to later style in stylesheet -->
 <section class="hero-image" <?php if ($bg_url) : ?>style="--background-image: url('<?php echo esc_url($bg_url); ?>');"<?php endif; ?>>
     <div class="hero-content text-white">
         <h1><?php woocommerce_page_title(); ?></h1>
     </div>
 </section>
 
-<div class="shop-layout-container">
+<!-- sidebar -->
+<main class="shop-layout-container">
     <!-- making the SIDEBAR -->
      <aside class="shop">
         <?php
---
+++ woocommerce/woocommerce.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+<main class="woocommerce">
+    <section class="woocommerce-container">
+        <?php woocommerce_content(); ?>
+    </section>
+</main>
+<?php get_footer(); ?>
