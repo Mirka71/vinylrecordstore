a7c86ada3036057c6304084e64c80e6e76cd75cd
Mirka71 <200627729@student.georgianc.on.ca>
2026-04-07 19:26:14 -0400

    added bootstrap/css links to functions.php and made a css file

--- functions.php
+++ functions.php
@@ -14,7 +14,16 @@ add_theme_support('post-thumbnails');
 
 // add stylesheet to function
 function my_theme_enqueue_assets(){
-    get_template_directory_uri() . '/style.css'
+    wp_enqueue_style('bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css', array(), '5.3.0', 'all' );
+     wp_enqueue_style(
+      'my-custom-style',
+      get_template_directory_uri() . '/assets/css/custom-style.css',
+      array('bootstrap-css'),
+      '1.0.0',
+      'all'
+    );
+    // Enqueue the Bootstrap JS
+    wp_enqueue_script( 'bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js', array('jquery'), '5.3.0', true );
 }
 add_action('wp_enqueue_scripts', 'my_theme_enqueue_assets');
 
