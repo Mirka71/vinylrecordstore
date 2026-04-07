bf72b12910f2a76e101807c7b30cf799c89ebceb
Mirka71 <200627729@student.georgianc.on.ca>
2026-04-07 19:16:20 -0400

    Made the single.php template to display blog posts

--- single.php
+++ single.php
@@ -2,7 +2,7 @@
 get_header();
 ?>
 
-main class="container">
+<main class="container">
 
 <?php
 
@@ -16,3 +16,30 @@ if(have_posts()) :
  <!-- Post title -->
   <section class="heading">
     <h1><?php the_title(); ?></h1>
+
+    <div class="post-meta">
+        <p>Posted on: <?php the_date(); ?></p>
+        <p>By: <?php the_author(); ?></p>
+
+        <!-- Categories -->
+        <p>Genre: <?php the_category(', '); ?></p>
+
+        <!-- Tags -->
+         <p>Tags: <?php the_tags('', ', '); ?></p>
+    </div>
+ </section>
+                <?php if (has_post_thumbnail()): ?>
+                    <div class="post-thumbnail">
+                        <?php the_post_thumbnail('large'); // Automatically generates <img> tag  ?>
+                    </div>
+                <?php endif; ?>
+                <section class="entry-content">
+                    <?php the_content(); ?>
+                </section>
+            </article>
+<?php 
+            endwhile;
+        endif;
+    ?>        
+</main>
+<?php get_footer(); ?>
