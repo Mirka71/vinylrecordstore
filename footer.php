ae6e797140cdf147bb039950d80b73c799b2996c
Mirka71 <200627729@student.georgianc.on.ca>
2026-04-06 21:23:32 -0400

    added widget areas two, three, and four as well as a copyright for my store

--- footer.php
+++ footer.php
@@ -4,4 +4,34 @@
     <!-- AREA ONE LOGO -->
      <div class="first widget-area">
         <a href="<?php echo esc_url(home_url()); ?>">
-            
+            <?php dynamic_sidebar('footer-widget-area-one'); ?>
+        </a>
+    </div>
+
+    <!-- AREA TWO HOME -->
+      <div class="second widget-area">
+            <?php dynamic_sidebar('footer-widget-area-two'); ?>
+        </a>
+    </div>
+
+      <!-- AREA THREE ABOUT -->
+      <div class="third widget-area">
+            <?php dynamic_sidebar('footer-widget-area-three'); ?>
+        </a>
+    </div>
+
+          <!-- AREA FOUR CONTACT -->
+      <div class="fourth widget-area">
+            <?php dynamic_sidebar('footer-widget-area-four'); ?>
+        </a>
+    </div>
+</section>
+
+<!-- COPYRIGHT -->
+ <section class="copyright">
+    <p>Vinyl Record Store. All Rights Reserved.</p>
+</section>
+
+</footer>
+</body>
+</html>
